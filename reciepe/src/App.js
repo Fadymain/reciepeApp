c83cdc7722ab6c809549612bacdaa756c5899268
Adetola <adetolafadairo@gmail.com>
2022-05-04 13:26:34 -0700

    cleaning up code

--- reciepe/src/App.js
+++ reciepe/src/App.js
@@ -10,7 +10,7 @@ function App() {
 
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState("");
-  const [query, setQuery] = useState("chicken");
+  const [query, setQuery] = useState("banana");
 
 
 
@@ -22,6 +22,7 @@ function App() {
     const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
+    console.log("data", data.hits);
   }
 
   const updateSearch = e => {
